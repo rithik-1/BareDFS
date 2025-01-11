7f64be552f614c8c36e3538e4b79ccbefddfdd15
Rithik Sureka <rithiksureka@microsoft.com>
2025-01-11 23:45:01 +0530

    Fix the issues with the Common & Client projects

---
+++ Common/Models/DataNode/DataNodeData.cs
@@ -0,0 +1,12 @@
+namespace BareDFS.Common.Models
+{
+    using System;
+
+    // Data model for the file contents
+    // Can be extended to include custom data types
+    [Serializable]
+    public class DataNodeData
+    {
+        public string Data { get; set; }
+    }
+}
